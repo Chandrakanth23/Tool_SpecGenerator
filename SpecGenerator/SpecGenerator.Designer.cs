06a7d5a7b2eb1fe6577a7d3ed3cc870071a00752
Chandrakanth23 <karlapatichandrakanth07@gmail.com>
2025-01-03 18:26:25 +0530

    Project Created

---
+++ SpecGenerator/SpecGenerator.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace SpecGenerator
+{
+    partial class SpecGenerator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // SpecGenerator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 608);
+            this.Name = "SpecGenerator";
+            this.Text = "Specification Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
